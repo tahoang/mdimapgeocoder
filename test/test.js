ce28ed7aad0cb84e441f177a4e7b0f581c5d5a34
Frank Rowe <fsrowe@gmail.com>
2014-08-18 14:12:16 -0400

    build

--- test/test.js
+++ test/test.js
@@ -1,14 +1,12 @@
-var Geocoder = require('../index.js')
+var MDiMapGeocoder = require('../index.js')
   , test = require("tap").test
-
-var geocoder = new Geocoder()
 
 function cb(err, res){
   console.log(err, res.candidates[0])
 }
 
-geocoder.search('1101 Camden Ave, Salisbury MD 21801', cb)
-geocoder.search({
+MDiMapGeocoder.search('1101 Camden Ave, Salisbury MD 21801', cb)
+MDiMapGeocoder.search({
   Street: '1101 Camden Ave',
   City: 'Salisbury',
   State: 'MD',
